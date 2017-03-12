690cd2b003059450b2c1ea8a77deaa49bff18d28
Dmitry Kalyuk <kalyuk@shopmaek.ru>
2017-03-12 13:53:45 +0700

    remove dist

--- dist/helpers/string.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-exports.ucfirst = ucfirst;
-function ucfirst(str) {
-	return str.charAt(0).toUpperCase() + str.substr(1, str.length - 1);
-}
